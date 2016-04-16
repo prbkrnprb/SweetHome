204c849c915c74dd7147b068547df5123301b805
prbkrnprb <mailme@prabakaran.in>
2016-04-16 14:36:25 +0530

    Initial commit

---
+++ app/src/main/java/in/prabakaran/sweethome/picaso/GalleryActivity.java
@@ -0,0 +1,15 @@
+package in.prabakaran.sweethome.picaso;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import in.prabakaran.sweethome.R;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+    }
+}
